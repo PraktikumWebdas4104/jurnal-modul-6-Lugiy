58f0bc1296b63ed76d8f457b1645b537f012fa3d
Ugiy <43519843+Lugiy@users.noreply.github.com>
2018-10-12 10:12:15 +0700

    lugi
    lugi

---
+++ DB6.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 12 Okt 2018 pada 05.08
+-- Versi server: 10.1.34-MariaDB
+-- Versi PHP: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jurnal6`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jurnal6`
+--
+
+CREATE TABLE `jurnal6` (
+  `nama` varchar(35) NOT NULL,
+  `nim` int(10) NOT NULL,
+  `kelas` varchar(30) NOT NULL,
+  `kelamin` int(30) NOT NULL,
+  `hobi` varchar(30) NOT NULL,
+  `fakultas` varchar(30) NOT NULL,
+  `alamat` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
